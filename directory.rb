221e37a4470b49902f96bb48dc1480bda6ef45de
Alasdair Wem <alasdair123@hotmail.co.uk>
2019-03-21 16:07:33 +0000

    add array and iterate

--- directory.rb
+++ directory.rb
@@ -1,17 +1,21 @@
 
-student_count = 11
+students = [
+ "Dr. Hannibal Lecter",
+ "Darth Vader",
+ "Nurse Ratched",
+ "Michael Corleone",
+ "Alex DeLarge",
+ "The Wicked Witch of the West",
+ "Terminator",
+ "Freddy Krueger",
+ "The Joker",
+ "Joffrey Baratheon",
+ "Norman Bates"
+]
+
 puts "The Students of Villain Academy"
 puts "-------------------------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
 
-puts "overall we have #{student_count} great students"
+students.each do |x| puts x end
+
+puts "Overall we have #{students.count} great students"
