539e2385575b74bdb94f9c73ef96ed5262a2c1c5
Alasdair Wem <alasdair123@hotmail.co.uk>
2019-03-21 15:31:29 +0000

    directory names added

---
+++ directory.rb
@@ -0,0 +1,12 @@
+
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Jeffrey Baratheon"
+puts "Norman Bates"
