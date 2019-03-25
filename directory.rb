f0eb23ec78a22dec025e5d54b9df39bebc988cea
Alasdair Wem <alasdair123@hotmail.co.uk>
2019-03-25 15:45:02 +0000

    make directory user input based

--- directory.rb
+++ directory.rb
@@ -1,31 +1,32 @@
 
-students = [
- "Dr. Hannibal Lecter",
- "Darth Vader",
- "Nurse Ratched",
- "Michael Corleone",
- "Alex DeLarge",
- "The Wicked Witch of the West",
- "Terminator",
- "Freddy Krueger",
- "The Joker",
- "Joffrey Baratheon",
- "Norman Bates"
-]
 
 def print_header
 puts "The Students of Villain Academy"
 puts "-------------------------------"
 end
 
-def print_students(names)
-names.each do |name| puts name end
+def print_students(students)
+students.each do |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" end
 end
 
 def print_footer(students)
 puts "Overall we have #{students.count} great students"
 end
 
+def input_students
+  puts "Please enter a student"
+  puts "To return the current directory, hit enter twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "no we have #{students.count} students"
+    name = gets.chomp
+  end
+    students
+end
+
+students = input_students
 print_header
-print(students)
+print_students(students)
 print_footer(students)
