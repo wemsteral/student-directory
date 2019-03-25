23a260d0d37fc5ea16d8481c058cea434ba042f3
Alasdair Wem <alasdair123@hotmail.co.uk>
2019-03-25 16:47:03 +0000

    menu and methods refactored

--- directory.rb
+++ directory.rb
@@ -1,32 +1,60 @@
+@students = []
 
+
+def interactive_menu
+  loop do
+    print print_menu
+    process(gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
+  when "1"
+    students = input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
 
 def print_header
-puts "The Students of Villain Academy"
-puts "-------------------------------"
+  puts "The Students of Villain Academy"
+  puts "-------------------------------"
 end
 
-def print_students(students)
-students.each do |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" end
+def print_students_list
+  @students.each do |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" end
 end
 
-def print_footer(students)
-puts "Overall we have #{students.count} great students"
+def print_footer
+  puts "Overall we have #{@students.count} great students"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
 def input_students
   puts "Please enter a student"
   puts "To return the current directory, hit enter twice"
-  students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "no we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "no we have #{@students.count} students"
     name = gets.chomp
   end
-    students
 end
 
-students = input_students
-print_header
-print_students(students)
-print_footer(students)
+interactive_menu
