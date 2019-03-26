775c0b44a515ae43eefa9f9755ae1dc05f38d227
Alasdair Wem <alasdair123@hotmail.co.uk>
2019-03-26 15:32:06 +0000

    dd search by first letter and add save to csv method

--- directory.rb
+++ directory.rb
@@ -8,12 +8,24 @@ def interactive_menu
   end
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Search student by first letter of name"
+  puts "4. Save students"
+  puts "9. Exit"
+end
+
 def process(selection)
   case selection
   when "1"
     students = input_students
   when "2"
     show_students
+  when "3"
+    name_by_letter
+  when "4"
+    save_students
   when "9"
     exit
   else
@@ -21,11 +33,7 @@ def process(selection)
   end
 end
 
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit"
-end
+# print out methods
 
 def print_header
   puts "The Students of Villain Academy"
@@ -33,11 +41,17 @@ def print_header
 end
 
 def print_students_list
-  @students.each do |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" end
+  @students.each.with_index(1) do |student, index| puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)" end
 end
 
 def print_footer
+  if @students.length < 0
+  puts "We don not have any students"
+  elsif @students.length == 1
+  puts "Overall we have #{@students.count} great student"
+  else
   puts "Overall we have #{@students.count} great students"
+  end
 end
 
 def show_students
@@ -46,15 +60,47 @@ def show_students
   print_footer
 end
 
+# input student method
+
 def input_students
   puts "Please enter a student"
   puts "To return the current directory, hit enter twice"
   name = gets.chomp
   while !name.empty? do
     @students << {name: name, cohort: :november}
-    puts "no we have #{@students.count} students"
+    if @students.length == 1
+    puts "now we have #{@students.count} student"
+    else
+    puts "now we have #{@students.count} students"
+    end
     name = gets.chomp
   end
 end
+
+# saving students to CSV method
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end 
+
+# name by first letter method
+
+def name_by_letter
+  puts "Please enter a letter to search (first name)"
+  puts "To return the current directory, hit enter twice"
+  letter = gets.chomp.upcase
+    @students.each do |student|
+      if letter == student[:name][0]
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+      end
+    end
+end
+
 
 interactive_menu
